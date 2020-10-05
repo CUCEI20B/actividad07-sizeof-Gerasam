7296910d844995fd07dc3ac6bfb7377ec47c9684
Gerardo Vazquez <gerardo.vazquez9084@alumnos.udg.mx>
2020-10-05 12:12:17 -0500

    Terminado

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 
+
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cad;
+    do
+    {
+        cout<<"\n\nIngrese 'salir' para cerrar el progarma\n";
+        cout<<"Ingrese tipo de dato: ";
+        getline(cin,cad);
+
+        if (cad=="int")
+        {
+            cout<<"Bytes: "<<sizeof(int);
+        }
+        else if (cad=="float")
+        {
+            cout<<"Bytes: "<<sizeof(float);
+        }
+        else if (cad=="char")
+        {
+            cout<<"Bytes: "<<sizeof(char);
+        }
+        else if (cad=="short")
+        {
+            cout<<"Bytes: "<<sizeof(short);
+        }
+        else if (cad=="double")
+        {
+            cout<<"Bytes: "<<sizeof(double);
+        }
+        else if (cad=="long")
+        {
+            cout<<"Bytes: "<<sizeof(long);
+        }
+    } while (cad!="salir");
+    
 
     return 0;
 }
